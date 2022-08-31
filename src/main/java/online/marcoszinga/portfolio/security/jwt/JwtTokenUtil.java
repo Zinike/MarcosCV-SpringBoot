a700693f59a9fae99f6a520fe6e3b637fe8b3470
Zinike <marcoszinga_1994@hotmail.com>
2022-08-31 20:56:23 -0300

    make it better

--- src/main/java/online/marcoszinga/portfolio/security/jwt/JwtTokenUtil.java
+++ src/main/java/online/marcoszinga/portfolio/security/jwt/JwtTokenUtil.java
@@ -19,7 +19,7 @@ import online.marcoszinga.portfolio.model.LoginModel;
 @Component
 public class JwtTokenUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(JwtTokenUtil.class);
-    private static final long EXPIRE_DURATION = 24 * 60 * 60 * 1000;
+    private static final long EXPIRE_DURATION = 60 * 1000;
     @Value("${app.jwt.secret}")
     private String secretKey;
 
