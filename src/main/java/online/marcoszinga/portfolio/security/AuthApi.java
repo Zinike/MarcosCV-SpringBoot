034418ab6840f1e6b64f42be13d6dfd89229d77c
Zinike <marcoszinga_1994@hotmail.com>
2022-08-31 00:57:14 -0300

    asd

--- src/main/java/online/marcoszinga/portfolio/security/AuthApi.java
+++ src/main/java/online/marcoszinga/portfolio/security/AuthApi.java
@@ -1,13 +1,16 @@
 package online.marcoszinga.portfolio.security;
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import online.marcoszinga.portfolio.model.LoginModel;
 import online.marcoszinga.portfolio.security.jwt.JwtTokenUtil;
 
 @RestController
@@ -17,16 +20,18 @@ public class AuthApi {
     AuthenticationManager authManager;
     @Autowired
     JwtTokenUtil jwtTokenUtil;
+
     @PostMapping("/api/Login")
     public ResponseEntity<?> login(@RequestBody @Valid AuthRequest request){
-        new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
+        try{
+            Authentication authentication=authManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());)
+            LoginModel user = (LoginModel) authentication.getPrincipal();
+            String accessToken = jwtTokenUtil.generateAccessToken(loginModel);
+            AuthResponse response = new AuthResponse(user.getEmail(), accessToken);
 
+            return ResponseEntity.ok().body(response);        
+        } catch (BadCredentialsException ex){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED.build());
+        }
     }
-
-
-
-
-
-
-
 }
