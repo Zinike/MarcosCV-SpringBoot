3db005283a65f8290dff8a0e43fdbf7eaf8106ca
Zinike <marcoszinga_1994@hotmail.com>
2022-08-31 00:28:30 -0300

    asd

--- src/main/java/online/marcoszinga/portfolio/ApplicationSecurityConfiguration.java
+++ src/main/java/online/marcoszinga/portfolio/ApplicationSecurityConfiguration.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import online.marcoszinga.portfolio.repository.LoginRepository;
 
 @Configuration
 @EnableWebSecurity
+@Deprecated
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter{
 
--- src/main/java/online/marcoszinga/portfolio/repository/LoginRepository.java
+++ src/main/java/online/marcoszinga/portfolio/repository/LoginRepository.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import online.marcoszinga.portfolio.model.LoginModel;
+
 
 public interface LoginRepository extends JpaRepository<LoginModel, Integer> {
     Optional<LoginModel> findByEmail(String email);
