4da2d576bb545a4f705e8f4505bebbc8c7e1cc9e
Zinike <marcoszinga_1994@hotmail.com>
2022-08-31 11:08:27 -0300

    asd

--- src/main/java/online/marcoszinga/portfolio/ApplicationSecurityConfiguration.java
+++ src/main/java/online/marcoszinga/portfolio/ApplicationSecurityConfiguration.java
@@ -1,12 +1,16 @@
 package online.marcoszinga.portfolio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import online.marcoszinga.portfolio.repository.LoginRepository;
+import online.marcoszinga.portfolio.security.jwt.JwtTokenFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,4 +20,13 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
 
     @Autowired
     private LoginRepository loginRepository;
+    @Autowired
+    private JwtTokenFilter jwtTokenFilter;
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    
 }
--- src/main/java/online/marcoszinga/portfolio/security/AuthApi.java
+++ src/main/java/online/marcoszinga/portfolio/security/AuthApi.java
@@ -2,12 +2,13 @@ package online.marcoszinga.portfolio.security;
 
 import javax.validation.Valid;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,16 +27,18 @@ public class AuthApi {
     @PostMapping("/api/Login")
     public ResponseEntity<?> login(@RequestBody @Valid AuthRequest request){
         try{
-            Authentication authentication=authManager.authenticate(
+            Authentication authentication = authManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                    request.getEmail(), request.getPassword());)
+                    request.getEmail(), request.getPassword()));
+
             LoginModel loginModel = (LoginModel) authentication.getPrincipal();
             String accessToken = jwtTokenUtil.GenerateAccessToken(loginModel);
-            AuthResponse response = new AuthResponse(user.getEmail(), accessToken);
+            AuthResponse response = new AuthResponse(loginModel.getEmail(), accessToken);
 
-            return ResponseEntity.ok().body(response);        
+            return ResponseEntity.ok().body(response);
+            
         } catch (BadCredentialsException ex){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED.build());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 }
