f49e3d532b562828a505913b9a761db0be0f117c
Zinike <marcoszinga_1994@hotmail.com>
2022-08-31 11:21:17 -0300

    asd

--- src/main/java/online/marcoszinga/portfolio/ApplicationSecurityConfiguration.java
+++ src/main/java/online/marcoszinga/portfolio/ApplicationSecurityConfiguration.java
@@ -3,11 +3,16 @@ package online.marcoszinga.portfolio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import online.marcoszinga.portfolio.repository.LoginRepository;
 import online.marcoszinga.portfolio.security.jwt.JwtTokenFilter;
@@ -28,5 +33,33 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
         return new BCryptPasswordEncoder();
     }
 
-    
+    @Override
+    protected void configure (AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(username -> loginRepository.findByEmail(username)
+        .orElseThrow(()-> new UsernameNotFoundException("NO USER")));
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        .authorizeRequests().antMatchers("/api/**")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic();
+
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 }
