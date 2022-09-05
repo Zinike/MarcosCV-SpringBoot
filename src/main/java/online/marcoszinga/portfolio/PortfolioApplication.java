1e5ea52452b11a05a37fd12d0986a87c37fc0987
Zinike <marcoszinga_1994@hotmail.com>
2022-09-05 13:36:31 -0300

    asd

--- src/main/java/online/marcoszinga/portfolio/PortfolioApplication.java
+++ src/main/java/online/marcoszinga/portfolio/PortfolioApplication.java
@@ -20,7 +20,7 @@ public class PortfolioApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://mycv-marcoszinga-32e08.web.app"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://mycv-marcoszinga-32e08.web.app", "http://localhost:4200"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
